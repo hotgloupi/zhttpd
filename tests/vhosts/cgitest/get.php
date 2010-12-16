d225c96ea23591f0cfcf08d05b52b7ce69e8bc59
Raphaël Londeix <raphael.londeix@gmail.com>
2010-12-16 12:30:40 +0100

    removing stupid tests directory

--- tests/vhosts/cgitest/get.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    echo "coucou";
-    if (isset($_GET['get']))
-        echo " " . $_GET['get'];
-?>
--- tests/vhosts/default/pif.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-file=image.jpg
-printf "Content-Type: image/jpeg\r\n"
-printf "Content-Disposition: filename=pif.jpg\r\n"
-printf "Content-Length: `du -b $file | cut -f1`\r\n\r\n"
-
-cat $file
-
